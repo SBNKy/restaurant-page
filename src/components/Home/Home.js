72f301b335d56c44cdcb10cd528c5a77912b14e2
Sebastian Konieczny <s.konieczny3211@gmail.com>
2026-01-27 23:30:22 +0100

    refactor: add functions to create sections

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,44 @@
 import "./style.css";
-import freshFood from "../../assets/images/freshFood.jpg";
+import freshFoodImg from "../../assets/images/freshFood.jpg";
 import cookingPhoto from "../../assets/images/cooking-photo.jpg";
+
 
 export default function createHomepage() {
     const container = document.createElement("div");
     container.classList.add("container");
-    
-    function createSeparator() {
-        const separatorLine = document.createElement("div");
-        separatorLine.classList.add("separator-line");
-        return separatorLine;
-    }
 
+    const welcomeTitle = "Welcome to {your restaurant name}";
+    const welcomeText = "A place where your expectations meet reality";
+    const welcomeDiv = createWelcomeMessage(welcomeTitle, welcomeText);
+
+    const foodSectionDescTitle = "The Art of Freshness";
+    const foodSectionDescText =
+        "We believe that the best dishes start long before they reach the kitchen. That’s why we partner with local farmers to source organic, sun-ripened ingredients harvested at their peak. No shortcuts, no preservatives—just the pure, vibrant taste of nature on your plate. Taste the difference that true quality makes.";
+    const foodSectionImgSrc = freshFoodImg;
+
+    const foodSection = createSection(
+        foodSectionDescTitle,
+        foodSectionDescText,
+        foodSectionImgSrc,
+        false,
+    );
+
+    const teamSectionDescTitle = "Crafted with Care";
+    const teamSectionDescText = "Behind every exquisite plate is a team of culinary artists obsessed with perfection. We believe that great food takes time, patience, and a steady hand. From the precise cut of the vegetables to the final garnish, our chefs pour their heart into every detail, ensuring that each bite tells a story of passion and mastery.";
+    const teamSectionImgSrc = cookingPhoto;
+
+    const teamSection = createSection(teamSectionDescTitle, teamSectionDescText, teamSectionImgSrc);
+
+    container.appendChild(welcomeDiv);
+    container.appendChild(createSeparator());
+    container.appendChild(foodSection);
+    container.appendChild(createSeparator());
+    container.appendChild(teamSection);
+
+    return container;
+}
+
+function createWelcomeMessage(title, text) {
     const welcomeDiv = document.createElement("div");
     welcomeDiv.classList.add("welcome-container");
     const welcomeText = document.createElement("h1");
@@ -25,55 +52,41 @@ export default function createHomepage() {
     welcomeDiv.appendChild(welcomeText);
     welcomeDiv.appendChild(welcomeDescription);
 
-    const foodSection = document.createElement("div");
-    foodSection.classList.add("grid");
+    return welcomeDiv;
+}
 
-    const freshFoodImage = document.createElement("img");
-    freshFoodImage.classList.add("image");
-    freshFoodImage.src = freshFood;
-    freshFoodImage.height = 400;
+function createSection(descTitle, descText, imgSrc, imgOnRight = true) {
+    const section = document.createElement("div");
+    section.classList.add("grid");
 
-    const foodDescription = document.createElement("div");
-    foodDescription.classList.add("description");
-    const foodDescriptionTitle = document.createElement("h2");
-    foodDescriptionTitle.textContent = "The Art of Freshness";
-    const foodDescriptionText = document.createElement("p");
-    foodDescriptionText.textContent =
-        "We believe that the best dishes start long before they reach the kitchen. That’s why we partner with local farmers to source organic, sun-ripened ingredients harvested at their peak. No shortcuts, no preservatives—just the pure, vibrant taste of nature on your plate. Taste the difference that true quality makes.";
+    const image = document.createElement("img");
+    image.classList.add("image");
+    image.src = imgSrc;
+    image.height = 400;
 
-    foodDescription.appendChild(foodDescriptionTitle);
-    foodDescription.appendChild(foodDescriptionText);
+    const description = document.createElement("div");
+    description.classList.add("description");
+    const descriptionTitle = document.createElement("h2");
+    descriptionTitle.textContent = descTitle;
+    const descriptionText = document.createElement("p");
+    descriptionText.textContent = descText;
 
-    foodSection.appendChild(freshFoodImage);
-    foodSection.appendChild(foodDescription);
+    description.appendChild(descriptionTitle);
+    description.appendChild(descriptionText);
 
-    const teamSection = document.createElement("div");
-    teamSection.classList.add("grid");
+    if (imgOnRight) {
+        section.appendChild(description);
+        section.appendChild(image);
+    } else {
+        section.appendChild(image);
+        section.appendChild(description);
+    }
 
-    const teamDescription = document.createElement("div");
-    teamDescription.classList.add("description");
-    const teamDescriptionTitle = document.createElement("h2");
-    teamDescriptionTitle.textContent = "Crafted with Care";
-    const teamDescriptionText = document.createElement("p");
-    teamDescriptionText.textContent =
-        "Behind every exquisite plate is a team of culinary artists obsessed with perfection. We believe that great food takes time, patience, and a steady hand. From the precise cut of the vegetables to the final garnish, our chefs pour their heart into every detail, ensuring that each bite tells a story of passion and mastery.";
+    return section;
+}
 
-    teamDescription.appendChild(teamDescriptionTitle);
-    teamDescription.appendChild(teamDescriptionText);
-
-    const teamImage = document.createElement("img");
-    teamImage.classList.add("image");
-    teamImage.src = cookingPhoto;
-    teamImage.height = 400;
-
-    teamSection.appendChild(teamDescription);
-    teamSection.appendChild(teamImage);
-
-    container.appendChild(welcomeDiv);
-    container.appendChild(createSeparator());
-    container.appendChild(foodSection);
-    container.appendChild(createSeparator());
-    container.appendChild(teamSection);
-
-    return container;
+function createSeparator() {
+    const separatorLine = document.createElement("div");
+    separatorLine.classList.add("separator-line");
+    return separatorLine;
 }

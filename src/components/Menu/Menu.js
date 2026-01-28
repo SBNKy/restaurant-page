e4a66dbb6c9e2ed3f5ac87b01cd29d14830a92fe
Sebastian Konieczny <s.konieczny3211@gmail.com>
2026-01-28 16:18:52 +0100

    feat: add 'menu component' files and 'create' function

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,8 @@
+import "./style.css"
+
+export default function createMenu() {
+    const container = document.createElement("div");
+    container.classList.add("container");
+
+    return container;   
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import "./style.css";
 import createHome from "./components/Home/Home";
+import createMenu from "./components/Menu/Menu";
 
 const contentContainer = document.querySelector("main");
+const homeBtn = document.querySelector("#home-button");
+const menuBtn = document.querySelector("#menu-button");
+const contactBtn = document.querySelector("#contact-button");
 
 function loadComponent(component) {
     contentContainer.innerHTML = "";
     contentContainer.appendChild(component);
 }
 
-const homeComponent = createHome();
+homeBtn.addEventListener("click", (e) => {
+    loadComponent(createHome());
+});
 
+
+const homeComponent = createHome();
 loadComponent(homeComponent)
